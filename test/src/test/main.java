ddaaf2fedc1a3cbe573aca0c10ae9085442d9402
omara <omara@DESKTOP-3TDV0PS>
2020-10-03 19:18:42 -0700

    first commit

---
+++ test/src/test/main.java
@@ -0,0 +1,9 @@
+package test;
+
+public class main {
+	public static void main(String[] args) {
+        System.out.println("Omar was here!");
+        System.out.println("This should work!! ");
+    }
+
+}
