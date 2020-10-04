9349e4058c5b4f444ca3addb92dc418a9123c4ae
omara <omara@DESKTOP-3TDV0PS>
2020-10-03 19:50:25 -0700

    Assignment02

---
+++ 360Assignment/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,32 @@
+/***
+ * Name: Omar Alsharif 
+ * Class: CSE 360
+ * Assignment02
+ */
+package cse360assignment02;
+
+public class AddingMachine {
+	 private int total;
+	  
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  // get total function
+	  public int getTotal () {
+	    return 0;
+	  }
+	  // add function
+	  public void add (int value) {
+	  }
+	  // subtract function
+	  public void subtract (int value) {
+	  }
+
+	  public String toString () {
+	    return "";
+	  }
+
+	  public void clear() {
+	  }
+
+}
