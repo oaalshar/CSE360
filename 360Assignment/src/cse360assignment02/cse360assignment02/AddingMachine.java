b7c7b909932a77c617166090aa7d138bfab260e5
omara <omara@DESKTOP-3TDV0PS>
2020-10-03 22:26:11 -0700

    Assignment02 part 2

--- 360Assignment/src/cse360assignment02/cse360assignment02/AddingMachine.java
+++ 360Assignment/src/cse360assignment02/cse360assignment02/AddingMachine.java
@@ -4,26 +4,39 @@
  * Assignment02
  */
 package cse360assignment02;
-
 public class AddingMachine {
+	public static void main(String Args[])
+	{
+		AddingMachine myCalculator = new AddingMachine();
+		myCalculator.add (4); 
+		myCalculator.subtract (2); 
+		myCalculator.add(5);
+		//Machine1.getTotal();
+		System.out.print(myCalculator);
+	}
 	 private int total;
-	  
+	 private StringBuffer history;
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    history = new StringBuffer("0");
 	  }
 	  // get total function
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  // add function
 	  public void add (int value) {
+		  total+=value;
+		  history.append(" + "+value);
 	  }
 	  // subtract function
 	  public void subtract (int value) {
+		  total = total - value;
+		  history.append(" - "+ value);
 	  }
 
 	  public String toString () {
-	    return "";
+		  return history.toString();
 	  }
 
 	  public void clear() {
