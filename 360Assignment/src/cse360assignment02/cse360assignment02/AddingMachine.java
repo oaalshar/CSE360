e720a515e9186a75617596877b9e8d8eca25ec67
omara <omara@DESKTOP-3TDV0PS>
2020-10-05 20:47:50 -0700

    Javadoc added

--- 360Assignment/src/cse360assignment02/cse360assignment02/AddingMachine.java
+++ 360Assignment/src/cse360assignment02/cse360assignment02/AddingMachine.java
@@ -1,45 +1,56 @@
-/***
+/**
  * Name: Omar Alsharif 
  * Class: CSE 360
  * Assignment02
  */
 package cse360assignment02;
 public class AddingMachine {
-	public static void main(String Args[])
-	{
-		AddingMachine myCalculator = new AddingMachine();
-		myCalculator.add (4); 
-		myCalculator.subtract (2); 
-		myCalculator.add(5);
-		//Machine1.getTotal();
-		System.out.print(myCalculator);
-	}
 	 private int total;
 	 private StringBuffer history;
-	  public AddingMachine () {
+	 /**
+	  * This is my constructor
+	  * it initializes total and history 
+	  */
+	 public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
 	    history = new StringBuffer("0");
 	  }
-	  // get total function
+	  /**
+	   * This is a function that returns the total value
+	   * @return
+	   * @param
+	   */
 	  public int getTotal () {
 	    return total;
 	  }
-	  // add function
+	  /**
+	   * This is a function that add parameter to the total value
+	   * @return
+	   * @param
+	   */
 	  public void add (int value) {
 		  total+=value;
 		  history.append(" + "+value);
 	  }
-	  // subtract function
+	  /**
+	   * This is a function that subtracts parameter from the total value
+	   * @return
+	   * @param
+	   */
 	  public void subtract (int value) {
 		  total = total - value;
 		  history.append(" - "+ value);
 	  }
-
+	  /**
+	   * This is a function that prints the history of all transactions
+	   * @return
+	   */
 	  public String toString () {
 		  return history.toString();
 	  }
 
 	  public void clear() {
+		  // java does its own memory garbage collection
 	  }
 
 }
